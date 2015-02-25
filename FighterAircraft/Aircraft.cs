2c9c6cd59e243ce04fb6bee8f77386db10d51fab
gwake1 <gwake1@gmail.com>
2015-02-25 10:02:34 -0600

    Created Domain Classes

---
+++ FighterAircraft/Aircraft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterAircraft
+{
+    class Aircraft
+    {
+        public int AircraftId { get; set; }
+        public string Name { get; set; }
+        public decimal Winspan { get; set; }
+        public string Description { get; set; }
+        public byte[] Photo { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
